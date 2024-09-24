0e543bd2cf50df4036e8999b8e53c5cc16034940
shanmathi-1420 <143080319+shanmathi-1420@users.noreply.github.com>
2024-09-24 22:46:35 +0530

    Updated

--- inc/dbinfo.inc.php
+++ inc/dbinfo.inc.php
@@ -3,5 +3,5 @@
 define('DB_SERVER', 'php-database.cfykkioo83tq.ap-south-1.rds.amazonaws.com');
 define('DB_USERNAME', 'admin');
 define('DB_PASSWORD', 'adminshannu');
-define('DB_DATABASE', 'php');
+define('DB_DATABASE', 'php-database');
 ?>
